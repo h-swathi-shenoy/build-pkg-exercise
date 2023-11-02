fc8014d0121ad9a383ca7c59154e61a47ccea17a
H Swathi Shenoy <hswathishenoy@gmail.com>
2023-11-02 07:47:28 +0530

    ready to use pkg

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='zip_read_yaml',
       version='0.1',
       description='Test the read of yaml files from zip folders',
-      url='#',
+      url='https://github.com/h-swathi-shenoy/build-pkg-exercise',
       author='hswathishenoy',
-      author_email='hswathishenoy@email.com',
+      author_email='hswathishenoy@gmail.com',
       license='MIT',
       packages=['zip_read_yaml'],
       zip_safe=False)
